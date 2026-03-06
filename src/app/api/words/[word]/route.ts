954b33209464858ad61e36f196a00daaf627ac80
parsherr <dogukanalpt@gmail.com>
2026-03-06 16:17:53 +0300

    route update

--- src/app/api/words/[word]/route.ts
+++ src/app/api/words/[word]/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(
     request: Request,
-    { params }: { params: { word: string } }
+    { params }: { params: Promise<{ word: string }> }
 ) {
     try {
         const { word } = await params;
